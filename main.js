af44fd07a0bda30b078ff480ee1b3740e80d4b74
“vatikhile” <vaibhawatikhile@gmail.com>
2021-01-29 09:15:44 +0530

    modify the code for checking the validation for password

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 console.log("---/* Welcome to User Registration */---");
 var readlineSync = require('readline-sync');
 const util = require("./utility");
-var fName = readlineSync.question( "Enter First name : ");
+var fName = readlineSync.question( "Enter First name : :");
 util.name(fName);
-var lName = readlineSync.question( "Enter Last name : ");
+var lName = readlineSync.question( "Enter Last name : :");
 util.name(lName);
-var email=readlineSync.question("Enter the emailAddress")
+var email=readlineSync.question("Enter the emailAddress : :")
 util.email(email)
-var number=readlineSync.question("Enter the Mobile Number")
+var number=readlineSync.question("Enter the Mobile Number : :")
 util.mobile(number)
+var password=readlineSync.question("Enter the password having min 8 character : :")
+util.password_check(password)
--- utility.js
+++ utility.js
@@ -8,7 +8,7 @@ class Utility {
         else {
             console.log(`Invalid ${firstName} Name`);
         }
-        return FIRST_NAME_PATTERN;
+        
 
     }
     email = (email) => {
@@ -20,20 +20,31 @@ class Utility {
         else {
             console.log("Invalid Email");
         }
-        return EMAIL_PATTERN;
+        
 
     }
-    mobile=(number)=>{
+    mobile = (number) => {
         let PHONE_NUMBER_PATTERN = /^([0-9]{2}[ ]+)?[0-9]{10}$/;
 
         if (number.match(PHONE_NUMBER_PATTERN)) {
-            console.log(`valid Phone Number${number}`);
+            console.log(`valid Phone Number::${number}`);
         }
         else {
-            console.log(`Invalid Phone Number${number}`);
+            console.log(`Invalid Phone Number::${number}`);
         }
-        return PHONE_NUMBER_PATTERN;
+      
 
+    }
+    password_check= (password) => {
+        let PASSWORD_PATTERN = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,40}$/
+
+        if (password.match(PASSWORD_PATTERN)) {
+            console.log(`valid Password::${password}`);
+        }
+        else{
+            console.log(`Invalid Password::${password}`);
+        }
+      
     }
 }
 module.exports = new Utility();
